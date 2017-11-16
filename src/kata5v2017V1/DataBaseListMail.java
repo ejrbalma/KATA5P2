983d4402a1362113a774d6d6c0f6d3ff9071458b
Enrique José Ramón Balmaseda <ejrbalma@gmail.com>
2017-11-16 09:52:10 +0000

    Segunda version KATA5P2

---
+++ src/kata5v2017V1/DataBaseListMail.java
@@ -0,0 +1,49 @@
+package kata5v2017V1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class DataBaseListMail {
+
+    private static Connection connection;
+    private static Statement statement;
+    private static ResultSet rs;
+    private final static String stringConnection = "org.sqlite.JDBC";
+    private final static String urlConnection = "jdbc:sqlite:/Users/ejrbalma/GIT/BBDD/KATA.sDB";
+
+    public static ArrayList<String> readDDBB() throws ClassNotFoundException, SQLException {
+
+        ArrayList<String> mailList = new ArrayList<>();
+        connect();
+
+        String query = "SELECT mail from mail";
+        ResultSet rs = statement.executeQuery(query);
+        while (rs.next()) {
+            if (rs.getString(1).contains("@")) {
+                mailList.add(rs.getString(1));
+            }
+        }
+
+        closeAll();
+        return mailList;
+    }
+
+    public static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName(stringConnection);
+        connection = DriverManager.getConnection(urlConnection);
+        statement = connection.createStatement();
+    }
+    
+    public static void closeAll() throws SQLException {
+        rs.close();
+        statement.close();
+        connection.close();
+    }
+    public ResultSet queryExecure (String query) throws SQLException {
+        return rs = statement.executeQuery(query);
+    }
+}
--- src/kata5v2017V1/KATA5V2017V1.java
+++ src/kata5v2017V1/KATA5V2017V1.java
@@ -1,13 +1,16 @@
 package kata5v2017V1;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class KATA5V2017V1 {
 
-    public static void main(String[] args) throws IOException {
-        String nameFile = "/Users/ejrbalma/Google Drive/Curso 2017 2018/ISII/KATA/Data/emailsfile.txt";
-        ArrayList <String> arrayMail = MailList.readFile(nameFile);
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+        
+        ArrayList <String> arrayMail = DataBaseListMail.readDDBB();
+        //String nameFile = "/Users/ejrbalma/Google Drive/Curso 2017 2018/ISII/KATA/Data/emailsfile.txt";
+        //ArrayList <String> arrayMail = MailList.readFile(nameFile);
         Histogram<String> histogram = MailHistogramBuilder.build(arrayMail);
         new HistogramDisplay(histogram).execute();
 
